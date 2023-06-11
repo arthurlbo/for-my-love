c4a0e9cd4e5331a60517231797f65bebf4a09d54
arthurlbo <arthurlbo16@gmail.com>
2023-06-11 14:52:54 -0300

    style: adding fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
 import "./globals.css";
+import { Concert_One as Concert, Poppins } from "next/font/google";
+
+const concert = Concert({ subsets: ["latin"], weight: "400", variable: "--font-concert" });
+const poppins = Poppins({ subsets: ["latin"], weight: "400", variable: "--font-poppins" });
 
 export const metadata = {
     title: "arthurlbo | Next TypeScript Template",
@@ -8,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className={`${concert.variable} ${poppins.variable}`}>{children}</body>
         </html>
     );
 }
