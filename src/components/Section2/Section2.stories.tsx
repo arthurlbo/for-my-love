201875bc744c94f35fe51afb7a9fa13006055d44
arthurlbo <arthurlbo16@gmail.com>
2023-06-11 15:32:56 -0300

    build: removing storybook

--- src/components/Section2/Section2.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryFn } from "@storybook/react";
-import { Section2 } from "./Section2";
-
-export default {
-    title: "Section2",
-    component: Section2,
-} as Meta;
-
-export const Default: StoryFn = () => <Section2 />;
